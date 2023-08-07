4d8e6503017753acd02fe04e0696c2d2bf5dab0f
Sebastian Marmol <smarmol@nubity.com>
2023-08-06 23:29:38 -0300

    Fix CORS and filters

--- src/main/java/com/sebas/trimixpeople/controllers/PersonController.java
+++ src/main/java/com/sebas/trimixpeople/controllers/PersonController.java
@@ -2,7 +2,7 @@ package com.sebas.trimixpeople.controllers;
 
 import com.sebas.trimixpeople.enums.Documento;
 import com.sebas.trimixpeople.exceptions.EntityValidationException;
-import com.sebas.trimixpeople.models.Person;
+import com.sebas.trimixpeople.models.Persona;
 import com.sebas.trimixpeople.services.PersonService;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
@@ -21,46 +21,72 @@ public class PersonController {
         this.personService = personService;
     }
 
+    private Response addCorsHeaders(Response.ResponseBuilder response) {
+        return response.header("Access-Control-Allow-Origin", "*")
+                .header("Access-Control-Allow-Methods", "GET, POST, DELETE, PATCH").header("Access-Control-Allow-Headers", "Content-Type").build();
+    }
+
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    //El challenge pide filtrar por nombre, opto que el mismo filtro se use como un OR en el apellido.
     public Response getPeople(@QueryParam("nombre") String nombre, @QueryParam("documento") Documento documento) {
-        return Response.ok(personService.getPeople(nombre, documento)).status(200).build();
+        return addCorsHeaders(Response.ok(personService.getPeople(nombre, documento)).status(200));
     }
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response insertPerson(Person person) {
+    public Response insertPerson(Persona person) {
         try {
-            return Response.ok(personService.insertPerson(person)).status(201).build();
+            return addCorsHeaders(Response.ok(personService.insertPerson(person)).status(201));
         } catch (EntityValidationException e) {
-            return Response.ok(e.getResponseDetail()).status(e.getStatus()).build();
+            return addCorsHeaders(Response.ok(e.getResponseDetail()).status(e.getStatus()));
         }
     }
 
     @PATCH
-    @Path("/{id}")
+    @Path("{id : \\d+}")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response updatePerson(Person person, @PathParam("id") Long id) {
+    public Response updatePerson(Persona person, @PathParam("id") Long id) {
         try {
-            return Response.ok(personService.updatePerson(id, person)).status(200).build();
+            return addCorsHeaders(Response.ok(personService.updatePerson(id, person)).status(200));
         } catch (EntityValidationException e) {
-            return Response.ok(e.getResponseDetail()).status(e.getStatus()).build();
+            return addCorsHeaders(Response.ok(e.getResponseDetail()).status(e.getStatus()));
         }
     }
 
     @DELETE
-    @Path("/{id}")
+    @Path("/{id : \\d+}")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public Response deletePerson(@PathParam("id") Long id) {
         try {
             personService.deletePerson(id);
         } catch (EntityValidationException e) {
-            return Response.ok(e.getResponseDetail()).status(e.getStatus()).build();
+            return addCorsHeaders(Response.ok(e.getResponseDetail()).status(e.getStatus()));
         }
-        return Response.status(200).build();
+        return addCorsHeaders(Response.status(200));
+    }
+
+    @OPTIONS
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response options() {
+        return getCorsHeaders();
+    }
+
+    @OPTIONS
+    @Path("/{id : \\d+}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response patchOptions() {
+        return getCorsHeaders();
+    }
+
+    private Response getCorsHeaders() {
+        return Response.ok("")
+                .header("Access-Control-Allow-Origin", "*")
+                .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
+                .header("Access-Control-Allow-Credentials", "true")
+                .header("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS")
+                .build();
     }
 }
